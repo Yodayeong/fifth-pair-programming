5e630c8820ac887a45f29161545b34da42353131
code-sum <yisugyeong@gmail.com>
2022-10-14 13:47:58 +0900

    회원정보 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
@@ -7,3 +7,9 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ['username', 'password1', 'password2']
         labels = {'username': '아이디', 'password1': '비밀번호', 'password2': '비밀번호확인'}
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
