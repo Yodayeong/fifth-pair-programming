b17405b1a178ba3226ab4cef85c17286388b5a77
yodayeong <duekdud01@naver.com>
2022-10-14 15:03:04 +0900

    update 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('<int:user_pk>/', views.detail, name='detail'),
-    path('update/', views.update, name='update'),
+    path('<int:user_pk>/update/', views.update, name='update'),
+    #path('password/', views.password, name='password'),
 ]
